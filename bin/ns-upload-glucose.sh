7d9456ba19c0d682027fa865b1ba23d21d8f46e2
cjo20 <git@cjo20.net>
2015-10-10 17:52:51 +0100

    Don't upload _tell to the server

--- bin/ns-upload-glucose.sh
+++ bin/ns-upload-glucose.sh
@@ -8,11 +8,13 @@ OUTPUT=${2-glucosehistory.ns.json}
 #TZ=${3-$(date +%z)}
 
 cat $HISTORY | \
-  json -e "this.medtronic = this._type;" | \
-  json -e "this.dateString = this.date + '$(date +%z)'" | \
-  json -e "this.date = new Date(this.dateString).getTime();" | \
-  json -e "this.type = (this.name == 'GlucoseSensorData') ? 'sgv' : 'pumpdata'" | \
-  json -e "this.device = 'openaps://medtronic/pump/cgm'" \
+  json -E "this.medtronic = this._type;" | \
+  json -E "this.dateString = this.date + '$(date +%z)'" | \
+  json -E "this.date = new Date(this.dateString).getTime();" | \
+  json -E "this.type = (this.name == 'GlucoseSensorData') ? 'sgv' : 'pumpdata'" | \
+  json -C "this.type == 'sgv'" | \
+  json -E "this.device = 'openaps://medtronic/pump/cgm'" | \
+  json -E "delete this._tell" \
   > $OUTPUT
 
 # requires API_SECRET and NIGHTSCOUT_HOST to be set in calling environment (i.e. in crontab)
